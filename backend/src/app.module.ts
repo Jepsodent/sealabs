f94b05c4f94f51b3cd281c2ebfba43429b099423
Jefferson <jefferson.swanto@gmail.com>
2026-06-22 15:29:33 +0700

    feat: add store (module, controller, service), dto for create update store

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { ConfigModule } from './config/config.module';
 import { AuthModule } from './auth/auth.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { ProductsModule } from './products/products.module';
+import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule, AuthModule, ReviewsModule, ProductsModule],
+  imports: [PrismaModule, ConfigModule, AuthModule, ReviewsModule, ProductsModule, StoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
